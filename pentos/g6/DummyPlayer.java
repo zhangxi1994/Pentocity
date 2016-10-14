9488b41105842de87adb79fa6fe395f67af142f4
DRC9702 <drinconcruz@knox.edu>
2016-10-13 23:14:49 -0400

    Dummy player.

--- pentos/g6/DummyPlayer.java
+++ pentos/g6/DummyPlayer.java
@@ -65,24 +65,43 @@ public class DummyPlayer implements pentos.sim.Player {
 			return new Move(false);
 
 		ArrayList<Move> moves2 = new ArrayList<Move>();
-		// Iterate through the moves and find their shortest roads for each
-		for (Move mc : moves) {
-			// get coordinates of building placement 
-			// (position plus local building cell coordinates)
-			Set<Cell> shiftedCells = new HashSet<Cell>();
-			for (Cell x : mc.request.rotations()[mc.rotation])
-				shiftedCells.add(new Cell(x.i + mc.location.i, x.j + mc.location.j));
-			// build a road to connect this building to perimeter
-			Set<Cell> roadCells = findShortestRoad(shiftedCells, land);
-			if (roadCells == null) {
-				if (isNextToRoad(mc.location.i, mc.location.j, mc.request, land)) {
+		boolean checkEmptyNeighborRoadAdj = true;
+		
+		while(true){
+			// Iterate through the moves and find their shortest roads for each
+			for (Move mc : moves) {
+				// get coordinates of building placement 
+				// (position plus local building cell coordinates)
+				Set<Cell> shiftedCells = new HashSet<Cell>();
+				for (Cell x : mc.request.rotations()[mc.rotation])
+					shiftedCells.add(new Cell(x.i + mc.location.i, x.j + mc.location.j));
+				// build a road to connect this building to perimeter
+				Set<Cell> roadCells = findShortestRoad(shiftedCells, land);
+//				if (roadCells.size()==0) {
+//					if (!isNextToRoad(mc.location.i, mc.location.j, mc.request, land)) {
+//						continue;
+//					}
+//				}
+				if(roadCells == null){
 					continue;
 				}
-			} else {
-				mc.road = roadCells;
-				moves2.add(mc);
+				else if(checkEmptyNeighborRoadAdj && !areNeighborsNextToRoad(mc.location.i, mc.location.j, mc.request, land, roadCells)){
+					continue;
+				}
+				else {
+					mc.road = roadCells;
+					moves2.add(mc);
+				}
+			}
+			if(checkEmptyNeighborRoadAdj && moves2.isEmpty()){
+				checkEmptyNeighborRoadAdj=false;
+				System.out.println("Failed using neighborsRoadChecking");
+			}
+			else{
+				break;
 			}
 		}
+		
 		
 		moves = moves2;
 
@@ -169,20 +188,99 @@ public class DummyPlayer implements pentos.sim.Player {
 			return output;
 	}
 
+	private boolean areNeighborsNextToRoad(int locI, int locJ, Building request, Land land, Set<Cell> road){
+		Iterator<Cell> cIt = request.iterator();
+		Set<Cell> emptyNeighbors = new HashSet<Cell>();
+		while (cIt.hasNext()) {
+			Cell c = cIt.next();
+			if (locJ + c.j + 1< land.side && land.getCellType(locI + c.i, locJ + c.j + 1) == Cell.Type.EMPTY)
+				emptyNeighbors.add(new Cell(locI + c.i, locJ + c.j + 1));
+			if (locJ + c.j - 1>= 0 && land.getCellType(locI + c.i, locJ + c.j - 1) == Cell.Type.EMPTY)
+				emptyNeighbors.add(new Cell(locI + c.i, locJ + c.j - 1));
+			if (locI + c.i + 1< land.side && land.getCellType(locI + c.i + 1, locJ + c.j) == Cell.Type.EMPTY)
+				emptyNeighbors.add(new Cell(locI + c.i + 1, locJ + c.j));
+			if (locI + c.i - 1>= 0 && land.getCellType(locI + c.i - 1, locJ + c.j) == Cell.Type.EMPTY)
+				emptyNeighbors.add(new Cell(locI + c.i - 1, locJ + c.j));
+		}
+//		for(Cell c : emptyNeighbors){
+//			if(!road.contains(c) && isNextToRoad(c.i, c.j, land)){
+//				if(	road.isEmpty() || isNextToSet(c.i, c.j, land, road) ){
+//					return true;
+//				}
+//			}
+//		}
+		for(Cell c : emptyNeighbors){
+			if(road.isEmpty() && isNextToRoad(c.i, c.j, land))
+				return true;
+			else if(!road.isEmpty() && setHas2EmptyNeighbors(road,land))
+				return true;
+		}
+		return false;
+	}
+	
+	private boolean setHas2EmptyNeighbors(Set<Cell> set, Land land){
+		Iterator<Cell> cIt = set.iterator();
+		int count=0;
+		while(cIt.hasNext()){
+			Cell c = cIt.next();
+			Cell c1, c2, c3, c4;
+			if(c.j+1 <= land.side){
+				c1 = new Cell(c.i, c.j+1);
+				if(!(c1.i < 0 || c1.i >= land.side || c1.j < 0 || c1.j >= land.side) && !set.contains(c1) && land.getCellType(c1)==Cell.Type.EMPTY)
+					count++;//return true;
+			}
+			if(c.j-1 > 0){
+				c2 = new Cell(c.i, c.j-1);
+				if(!(c2.i < 0 || c2.i >= land.side || c2.j < 0 || c2.j >= land.side) && !set.contains(c2) && land.getCellType(c2)==Cell.Type.EMPTY)
+					count++;//return true;
+			}
+			if(c.i + 1 <= land.side){
+				c3 = new Cell(c.i+1, c.j);
+				if(!(c3.i < 0 || c3.i >= land.side || c3.j < 0 || c3.j >= land.side) && !set.contains(c3) && land.getCellType(c3)==Cell.Type.EMPTY)
+					count++;//return true;
+			}
+			if(c.i-1 > 0){
+				c4 = new Cell(c.i-1, c.j);
+				if(!(c4.i < 0 || c4.i >= land.side || c4.j < 0 || c4.j >= land.side) && !set.contains(c4) &&land.getCellType(c4)==Cell.Type.EMPTY)
+					count++;//return true;
+			}
+
+		}
+		//System.out.println("Count is: " + count);
+		return (count>=2);//return false;
+	}
+	
 	private boolean isNextToRoad(int locI, int locJ, Building request, Land land) {
 		Iterator<Cell> cIt = request.iterator();
 		while (cIt.hasNext()) {
 			Cell c = cIt.next();
-			if (locJ + 1 >= land.side || land.getCellType(locI + c.i, locJ + c.j + 1) == Cell.Type.ROAD)
+			if (locJ + c.j+ 1 >= land.side || land.getCellType(locI + c.i, locJ + c.j + 1) == Cell.Type.ROAD)
 				return true;
-			if (locJ - 1 < 0 || land.getCellType(locI + c.i, locJ + c.j - 1) == Cell.Type.ROAD)
+			if (locJ + c.j - 1 < 0 || land.getCellType(locI + c.i, locJ + c.j - 1) == Cell.Type.ROAD)
 				return true;
-			if (locI + 1 >= land.side || land.getCellType(locI + c.i + 1, locJ + c.j) == Cell.Type.ROAD)
+			if (locI + c.i + 1 >= land.side || land.getCellType(locI + c.i + 1, locJ + c.j) == Cell.Type.ROAD)
 				return true;
-			if (locI - 1 < 0 || land.getCellType(locI + c.i - 1, locJ + c.j) == Cell.Type.ROAD)
+			if (locI + c.i - 1 < 0 || land.getCellType(locI + c.i - 1, locJ + c.j) == Cell.Type.ROAD)
 				return true;
 		}
 		return false;
+	}
+	
+	private boolean isNextToSet(int locI, int locJ, Land land, Set<Cell> set){
+		if (!(locJ < land.side) || !(locJ >= 0) || !(locI < land.side) || !(locI >= 0)	)
+			throw new RuntimeException("Bad cell");
+		else{ //Don't need to check if neighbor is a valid cell as long as set is only valid cells
+			Cell c1 = new Cell(locI, locJ+1);
+			Cell c2 = new Cell(locI, locJ-1);
+			Cell c3 = new Cell(locI+1, locJ);
+			Cell c4 = new Cell(locI-1, locJ);
+			if(set.contains(c1) || set.contains(c2) || set.contains(c3) || set.contains(c4)){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
 	}
 	
 	private boolean isNextToRoad(int locI, int locJ, Land land) {
